263c157c3579affcf690a07bb7250d890ea5e9bb
Leslie Joe <leslie.kojo.okyere@outlook.com>
2020-09-27 08:43:03 +0000

    Permissions and viewset added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+from profiles_api import serializers, models, permissions
+
+class HelloApiView(APIView):
+    """"Hello Api View"""
+    
+    serializer_class = serializers.HelloSerializer
+    
+    def get(self, request, format=None):
+        an_apiview = [
+            'Something goes here'
+        ]
+    
+        return Response({'an_apiview': an_apiview})
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            return Response({'message': f"Hello {name}"})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserProfileViewset(viewsets.ModelViewSet):
+    """User Profile Viewset API"""
+    queryset = models.UserProfile.objects.all()
+    serializer_class = serializers.UserProfileSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.UpdateOwnProfile, )
